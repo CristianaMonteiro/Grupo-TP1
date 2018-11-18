ea8a2984029c2d10d8d22f29a028605b77653a85
andreribeiroo <44165658+andreribeiroo@users.noreply.github.com>
2018-11-18 10:15:48 +0000

    Add files via upload
    Update project

---
+++ TP1/src/main/java/ALLTests.java
@@ -0,0 +1,2 @@
+public class ALLTests {
+}
---
+++ TP1/src/test/java/MyTests.java
@@ -0,0 +1,16 @@
+import Models.User;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+
+public class MyTests {
+
+    @Test
+public void getName(){
+        User person = new User(11, "andre",11);
+
+        String ResName = person.getName();
+        assertEquals("andre", ResName);
+    }
+}
