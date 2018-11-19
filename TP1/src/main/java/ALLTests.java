bed2eb6b4e20e6f6024e1d287200fb9d3432dd40
andreribeiroo <44165658+andreribeiroo@users.noreply.github.com>
2018-11-19 13:22:47 +0000

    Add files via upload
    Update class ALLTests

--- TP1/src/main/java/ALLTests.java
+++ TP1/src/main/java/ALLTests.java
@@ -1,2 +1,21 @@
+import Models.Bike;
+import Models.Deposit;
+import Models.Lock;
+import Models.User;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({
+        BikeRentalSystem.class,
+        User.class,
+        Deposit.class,
+        Lock.class,
+        Bike.class
+})
+
+
+
 public class ALLTests {
 }
