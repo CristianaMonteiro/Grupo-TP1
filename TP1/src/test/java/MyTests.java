74f0948462e3ba94d4592fd2de7e6b360e7defe4
andreribeiroo <44165658+andreribeiroo@users.noreply.github.com>
2018-11-19 21:09:41 +0000

    Add files via upload
    Update MyTests methods

--- TP1/src/test/java/MyTests.java
+++ TP1/src/test/java/MyTests.java
@@ -1,3 +1,4 @@
+
 import Exceptions.UserAlreadyExists;
 import Exceptions.UserDoesNotExists;
 import Models.User;
@@ -7,51 +8,72 @@ import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.Assert.assertEquals;
 
-
-
 public class MyTests {
+
     private BikeRentalSystem user;
 
     @BeforeEach
     public void setUp() throws UserAlreadyExists {
 
-
         user = new BikeRentalSystem(1);
         user.registerUser(30, "andre", 1);
 
     }
+
     @Test
-    public void getName(){
-        User person = new User(11, "andre",11);
+    public void getName() {
+        User person = new User(11, "andre", 11);
         String ResName = person.getName();
         assertEquals("andre", ResName);
-        //assertEquals("NULL", ResName);  //Teste com input -> NUll
+        //assertEquals("NULL", ResName);  //    Teste com input -> NUll
+    }
+
+    @Test
+    public void IDUser() {
+        User id = new User(2, "andre", 23);
+
+        assertEquals(id.getIDUser(), 2);
+
+        // assertEquals(id.getIDUser(), -6);   //   Teste com valores negativos!
     }
     @Test
-    public void IDUser(){
-        User id = new User(2,"andre",23);
+    public void verificaSeExisteID() {
 
-           assertEquals(id.getIDUser(), 2);
+        User id = new User(30, "andre", 1);
+        Assertions.assertThrows(UserAlreadyExists.class, () -> user.registerUser(30, "andre", 1));
 
-          // assertEquals(id.getIDUser(), -6);   //   Teste com valores negativos!
-}
-
-
+    }
     @Test
-    public void getBicycleTestUserInvalido() throws UserAlreadyExists, UserDoesNotExists {
+    public void VerifyActiveUser() throws UserAlreadyExists {
+                            //verifica quantos utilizadores estao ativos
 
+        int rentalFee = 5;
+
+        BikeRentalSystem user = new BikeRentalSystem(rentalFee);
+        user.registerUser(1, "andre", 1);
+        user.registerUser(2, "cristiana", 2);
+        user.registerUser(3, "nando", 3);
+
+        Assertions.assertEquals(3, user.getUsers().size());
+
+    }
+
+        @Test
+    public void getBicycleTestAllHypo() throws UserAlreadyExists, UserDoesNotExists {
 
         BikeRentalSystem user = new BikeRentalSystem(1);
 
         user.registerUser(2, "andre", 1);
 
+            //Verificar se o User com id 2 existe e possui um rental program
+            Assertions.assertThrows(UserAlreadyExists.class, () ->user.registerUser(2,"andre",1));
 
-        Assertions.assertThrows(UserDoesNotExists.class, () -> user.getBicycle(1, 1, 1));
-        Assertions.assertThrows(UserDoesNotExists.class, () -> user.getBicycle(1, 0, 1));
-        Assertions.assertThrows(UserDoesNotExists.class, () -> user.getBicycle(1, -1, 1));
+            //Não devem funcionar os testes abaixo, pois passamos IDUsers que nao estao atribuidos
+            Assertions.assertThrows(UserDoesNotExists.class, () -> user.getBicycle(1, 0, 1));
+            Assertions.assertThrows(UserDoesNotExists.class, () -> user.getBicycle(1, 1, 1));
+            Assertions.assertThrows(UserDoesNotExists.class, () -> user.getBicycle(1, -1, 1));
 
-
-    }
+        }
 
     @Test
     public void verifyCredit() throws UserAlreadyExists {
@@ -62,20 +84,8 @@ public class MyTests {
         user.registerUser(idUser, "andre", 1);
         user.addCredit(idUser, 10);
 
-
         Assertions.assertTrue(user.verifyCredit(idUser));
     }
-
-    @Test
-    public void verificaSeExisteID() {
-
-        User id = new User(30,"andre",1);
-        Assertions.assertThrows(UserAlreadyExists.class, () -> user.registerUser(30, "andre", 1));
-
-
-
-    }
-
 
 
 
